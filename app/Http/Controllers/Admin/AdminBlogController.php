f5644592c69d50deff555faebd7c040ee231081c
hataken <bb52119638@gmail.com>
2025-02-17 01:22:34 +0900

    section14.43

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Http\Requests\Admin\UpdateBlogRequest;
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminBlogController extends Controller
 {
@@ -59,9 +61,21 @@ class AdminBlogController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBlogRequest $request, string $id)
     {
-        //
+        $blog = Blog::findorFail($id);
+				$updateData = $request->validated();
+				
+				// 画像を変更する場合
+				if($request->has('image')) {
+					// 変更前の画像を削除
+					Storage::disk('public')->delete($blog->image);
+					// 変更後の画像をアップロード、保存パスを更新対象データにセット
+					$updateData['image'] = $request->file('image')->store('blogs', 'public');
+				}
+				$blog->update($updateData);
+
+				return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
     }
 
     /**
