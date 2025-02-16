3213a6d5149adc37b196104196dff346410511c8
hataken <bb52119638@gmail.com>
2025-02-16 17:18:44 +0900

    section14.41

---
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class AdminBlogController extends Controller
+{
+    // ブログ一覧画面
+    public function index()
+    {
+        return view('admin.blogs.index');
+    }
+
+
+    // ブログ投稿画面
+    public function create()
+    {
+        return view('admin.blogs.create');
+    }
+
+    // ブログ投稿処理
+    public function store(StoreBlogRequest $request)
+    {
+        $savedImagePath = $request->file('image')->store('blogs', 'public');
+        $blog = new Blog($request->validated());
+        $blog->image = $savedImagePath;
+        $blog->save();
+
+        // 下記処理でもOK
+        // $validated = $request->validated();
+        // $validated['image'] = $request->file('image')->store('blogs', 'public');
+        // Blog::create($validated);
+        
+        return to_route('admin.blogs.index')->with('success', 'ブログを投稿しました');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    // ブログ一覧画面
+    public function index()
+    {
+        return view('admin.blogs.index');
+    }
+
+
+    // ブログ投稿画面
+    public function create()
+    {
+        return view('admin.blogs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
